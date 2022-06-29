0ec5700e478c6a26d9dbbaad45961b41952cd1ba
Eric-Cortez <ecrcortez@gmail.com>
2022-06-29 12:51:20 -0700

    update config

--- sphinx/source/conf.py
+++ sphinx/source/conf.py
@@ -16,7 +16,14 @@ import sys
 
 # from pkg_resources import get_distribution
 
-sys.path.insert(0, os.path.abspath("../../"))
+_HERE = os.path.dirname(__file__)
+_ROOT_DIR = os.path.abspath(os.path.join(_HERE, '../..'))
+_PACKAGE_DIR = os.path.abspath(os.path.join(_HERE, '../../aepsych'))
+
+sys.path.insert(0, _ROOT_DIR)
+sys.path.insert(0, _PACKAGE_DIR)
+
+# sys.path.insert(0, os.path.abspath("../../"))
 
 # base_path = os.path.abspath(os.path.join(__file__, "..", "..", "..", "aepsych"))
 # print(sys.path, base_path, "======")
